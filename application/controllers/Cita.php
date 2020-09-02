a6d3052aeed36728e654c564d90580d1b47e4b8f
Luis Ricardo <luisls1717@gmail.com>
2020-09-02 16:42:42 -0500

    bug paciente

--- application/controllers/Cita.php
+++ application/controllers/Cita.php
@@ -8,7 +8,7 @@ class Cita extends CI_Controller {
 	public function __construct(){
     parent::__construct();
 		$this->load->helper(array('fechas_helper', 'otros_helper', 'imagen_helper'));
-		$this->load->model(array('model_cita', 'model_diagnostico'));
+		$this->load->model(array('model_cita', 'model_diagnostico', 'model_paciente'));
 
 		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, no-transform, max-age=0, post-check=0, pre-check=0");
 		$this->output->set_header("Pragma: no-cache");
