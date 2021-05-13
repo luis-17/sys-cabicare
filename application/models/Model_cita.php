eb99bd12fef2cce94ff2b1469fd90c3c1f30dede
Luis Ricardo <luisls1717@gmail.com>
2021-05-12 22:20:43 -0500

    correccion fecha

--- application/models/Model_cita.php
+++ application/models/Model_cita.php
@@ -912,7 +912,7 @@ class Model_cita extends CI_Model {
 		// if($params['origen']['id'] === 'EXT'){
 		// 	$this->db->where('pr.procedencia', 'EXT');
 		// }
-		$this->db->where("DATE(pg.fechaRegistro) BETWEEN '" . darFormatoYMD($params['desde']) ."' AND '" . darFormatoYMD($params['hasta'])."'");
+		$this->db->where("DATE(ci.fechaCita) BETWEEN '" . darFormatoYMD($params['desde']) ."' AND '" . darFormatoYMD($params['hasta'])."'");
 		$this->db->group_by('pg.metodoPago');
 		// if($params['orden']['id'] == 'OC'){
 		// 	$this->db->order_by('COUNT(*)', 'DESC');
